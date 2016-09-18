ab5e3be19596d33063f7e057522fa878bf1d5f12
vitek999 <vitek2012rus@gmail.com>
2016-09-18 10:10:10 +0300

    fix keypad,touch and LCM Table

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 13 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Sat Sep 17 16:00:56 MSK 2016"
+#define UTS_VERSION "#13 SMP PREEMPT Sun Sep 18 09:39:14 MSK 2016"
 #define LINUX_COMPILE_BY "viktornoskin"
 #define LINUX_COMPILE_HOST "HomePC"
 #define LINUX_COMPILER "gcc version 4.8 (GCC) "
